5427bf53866e9aeed2512603a166e2b2f9fc835a
Minkyu Kim <mok07333@gmail.com>
2025-06-08 20:36:21 +0900

    set up firebase

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbcWb9F_J5UpiY_9mL445rlpiSJ5WQr-s",
+  authDomain: "nwitter-7c62e.firebaseapp.com",
+  projectId: "nwitter-7c62e",
+  storageBucket: "nwitter-7c62e.firebasestorage.app",
+  messagingSenderId: "757390922295",
+  appId: "1:757390922295:web:b1d0efc61544c70c7de5b2",
+};
+
+const app = initializeApp(firebaseConfig);
